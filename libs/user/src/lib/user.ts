addda6c2faa03aa70389423daa1ead54bbe4373f
Advait Sharma <advsharm@partnet.cisco.com>
2021-04-14 14:46:56 +0530

    published packages change

--- libs/user/src/lib/user.ts
+++ libs/user/src/lib/user.ts
@@ -1,8 +1,9 @@
 const firstName = "testing";
 const lastName = "publish";
 const middleName = "none";
-const age = "24"
-const gender = "male"
-const address = "test"
+const age = "24";
+const gender = "male";
+const address = "test";
+const maritalStatus = "single";
 
-export {firstName, lastName, middleName, age, gender, address };
+export {firstName, lastName, middleName, age, gender, address, maritalStatus };
